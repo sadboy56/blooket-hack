d75e98c67b9b299d01f96dc9212e01d669b88360
sadboy56 <98235820+sadboy56@users.noreply.github.com>
2022-01-22 15:17:14 -0500

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -16,7 +16,7 @@ async function addCurrencies() {
     const tokens = Number(prompt('How many tokens do you want to add to your account? (500 daily)'));
 
     if (tokens > 500) {
-        alert('You can only add up to 500 tokens daily.');
+        alert('You can only add up to 10000 tokens daily.');
     };
 
     const response = await fetch('https://api.blooket.com/api/users/add-rewards', {
